88aa68309f1338858e3038d534945344332e7c94
Jesus <jesussi2012@gmail.com>
2026-09-17 07:05:16 +0200

    añadir interface

--- src/modelo.ts
+++ src/modelo.ts
@@ -1,5 +1,16 @@
-export let puntosTotales = 0;
 
-export const actualizarPuntosTotales = (puntosSumados: number): void => {
-  puntosTotales = puntosSumados;
+interface Partida{
+  puntosTotales:number;
+  
 };
+
+export const partida: Partida={
+puntosTotales:0,
+};
+
+export const crearPartida=():void{
+  partida.puntosTotales=0;
+};
+
+
+
